530d506454efba7c44268eec76d93a218b312171
jmjpickard <jack.pickard@carnallfarrar.com>
2021-09-07 12:58:16 +0100

    accelerator table component working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
-import _ from 'lodash';
+const d3 = require('d3');
+import './styles.css';
 
-function component(root) {
-    console.log(root)
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  root.appendChild(element)
+function component(root, data, columns) { 
+  console.log(root, data, columns);
+  const table = d3.select(root).append('table');
+  const thead = table.append('thead');
+  const tbody = table.append('tbody');
+    
+  // append the header row
+  thead.append('tr')
+    .selectAll('th')
+    .data(columns).enter()
+    .append('th')
+    .text(function (column) { return column; });
+    
+  // create a row for each object in the data
+  const rows = tbody.selectAll('tr')
+    .data(data)
+    .enter()
+    .append('tr');
+     
+  // create a cell in each row for each column
+  rows.selectAll('td')
+    .data(function (row) {
+      return columns.map(function (column) {
+  
+        return {column: column, value: row[column]};
+      });
+    })
+    .enter()
+    .append('td')
+    .html(function (d) { return d.value; });
+  
+  
+    
+  return table; 
 }
 
-global.component = component
+global.component = component; 
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,14 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 };
