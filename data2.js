9800ee6a63b5096d2309a275b6d804f875355563
Chris Monit <4059789+chrismonit@users.noreply.github.com>
2021-12-08 15:50:42 +0000

    comments

--- data2.js
+++ data2.js
@@ -10,6 +10,7 @@ sentencesShort = {
       'sentenceNumber': 2,
       'sentenceContent': 'Being Asian increases your odds of waiting longer than 4 hours in A&E by 12.2%. Cambridgeshire and Peterborough ranks 37 out of 42 systems for this metric.'
     }
-  ]
+  ] // ,
+  // ''   // TODO add another object here which contains the light documentation, but not the features used in the model, as this can be in the insight sentences dataset so it's more easily updated by backend devs
 }
   
