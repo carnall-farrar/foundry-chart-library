3dcba385679417301bbce6138a67e811c8f8d192
Alexandre Rieux <alexr.3165@gmail.com>
2022-03-16 15:19:29 +0000

    Update example to reflect prop change

--- src/likebutton.js
+++ src/likebutton.js
@@ -7,11 +7,12 @@ export class LikeButton extends React.Component {
   }
 
   render() {
+    console.log(this.props.count);
     if (this.state.liked) {
       return "You liked this.";
     }
 
-    return <button onClick={() => this.setState({ liked: true })}>Like</button>
+    return <button onClick={() => this.setState({ liked: true })}>Like</button>;
     // return e(
     //   "button",
     //   { onClick: () => this.setState({ liked: true }) },
