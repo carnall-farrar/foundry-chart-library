81a5905c82f00cacbfd140e483dae6e5b861f1c4
jmjpickard <jack.pickard@carnallfarrar.com>
2022-03-17 15:55:39 +0000

    adding example schema for new components

---
+++ newData.js
@@ -0,0 +1,126 @@
+scoreCard = {
+  inputData: [
+    {
+      'Performance Area': 'Activity',
+      'Metric': 'Value Weighted Activity (NHS)',
+      'Data last updated': 'February 10 2022',
+      '22/23 Ambition': '=/> 101%',
+      'April 22': '89%',
+      'May 22': '101%',
+      'June 22': '',
+      'July 22': '',
+      'Aug 22': '',
+      'Sept 22': '',
+      'Oct 22': '',
+      'Nov 22': '',
+      'Dec 22': '',
+      'Jan 22': '',
+      'Feb 22': '',
+      'March 22': '',
+      '6 week trend': [0.84, 0.94, 0.90, 0.87, 0.84, 0.80]
+    },
+    {
+      'Performance Area': 'Long Waits',
+      'Metric': 'Value Weighted Activity (IS)',
+      'Data last updated': 'February 10 2022',
+      '22/23 Ambition': '=/> 101%',
+      'April 22': '89%',
+      'May 22': '101%',
+      'June 22': '',
+      'July 22': '',
+      'Aug 22': '',
+      'Sept 22': '',
+      'Oct 22': '',
+      'Nov 22': '',
+      'Dec 22': '',
+      'Jan 22': '',
+      'Feb 22': '',
+      'March 22': '',
+      '6 week trend': [0.84, 0.94, 0.90, 0.87, 0.84, 0.80]
+    }
+  ],
+  columns: [
+    'Performance Area',
+    'Metric',
+    'Data last updated',
+    '22/23 Ambition',
+    'April 22',
+    'May 22',
+    'June 22',
+    'July 22',
+    'Aug 22',
+    'Sept 22',
+    'Oct 22',
+    'Nov 22',
+    'Dec 22',
+    'Jan 22',
+    'Feb 22',
+    'March 22',
+    '6 week trend'
+  ]
+};
+
+benchmark = {
+  inputData: [
+    {
+      '': 'Metric (target)',
+      'VWA (>/= 104%)': '83%',
+      'Completed Pathways (>/= 110%)': '90%',
+      'IS Activity (>/= 130%)': '101%',
+      'Outpatient Reduction (>/= 85%)': '94%',
+      'Wait to First Outpatient (TBC) -> not in data': '10 weeks',
+      '104ww (0)': '29,093',
+      '78ww (0)': '50,043',
+      'Cancer 62 Days (14,266)': '28,030',
+      'Diagnostic Test Activity (120%)': '97%'
+    },
+    {
+      '': 'Metric (target)',
+      'VWA (>/= 104%)': '83%',
+      'Completed Pathways (>/= 110%)': '90%',
+      'IS Activity (>/= 130%)': '101%',
+      'Outpatient Reduction (>/= 85%)': '94%',
+      'Wait to First Outpatient (TBC) -> not in data': '10 weeks',
+      '104ww (0)': '29,093',
+      '78ww (0)': '50,043',
+      'Cancer 62 Days (14,266)': '28,030',
+      'Diagnostic Test Activity (120%)': '97%'
+    },
+    {
+      '': 'Metric (target)',
+      'VWA (>/= 104%)': '83%',
+      'Completed Pathways (>/= 110%)': '90%',
+      'IS Activity (>/= 130%)': '101%',
+      'Outpatient Reduction (>/= 85%)': '94%',
+      'Wait to First Outpatient (TBC) -> not in data': '10 weeks',
+      '104ww (0)': '29,093',
+      '78ww (0)': '50,043',
+      'Cancer 62 Days (14,266)': '28,030',
+      'Diagnostic Test Activity (120%)': '97%'
+    }
+  ],
+  metricColumns: [
+    '',
+    'VWA (>/= 104%)',
+    'Completed Pathways (>/= 110%)',
+    'IS Activity (>/= 130%)',
+    'Outpatient Reduction (>/= 85%)',
+    'Wait to First Outpatient (TBC) -> not in data',
+    '104ww (0)',
+    '78ww (0)',
+    'Cancer 62 Days (14,266)',
+    'Diagnostic Test Activity (120%)'
+  ],
+  headerRow: {
+    '': '',
+    'VWA (>/= 104%)': 'Activity',
+    'Completed Pathways (>/= 110%)': 'Waiting List',
+    'IS Activity (>/= 130%)': 'Independent Sector',
+    'Outpatient Reduction (>/= 85%)': 'Outpatient Transformation',
+    'Wait to First Outpatient (TBC) -> not in data': 'Outpatient Transformation',
+    '104ww (0)': 'Long Waits',
+    '78ww (0)': 'Long Waits',
+    'Cancer 62 Days (14,266)': 'Cancer',
+    'Diagnostic Test Activity (120%)': 'Diagnostics'
+  }
+}
