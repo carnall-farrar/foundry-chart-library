439a647b44512ce202e497c4b793901a05bacb2f
zaharamiriam <miriam.zahara@carnallfarrar.com>
2022-03-22 11:28:16 +0000

    Add tooltip styles and test data

--- src/components/TrendLineChart.jsx
+++ src/components/TrendLineChart.jsx
@@ -12,7 +12,10 @@ const {
 export const TrendLineChart = ({ data, width, height }) => {
   return (
     <LineChart data={data} width={width} height={height}>
-      <Tooltip wrapperStyle={{ width: 50, height: 50 }} />
+      <Tooltip
+        wrapperStyle={{ width: 80, height: 50 }}
+        contentStyle={{ fontSize: 10 }}
+      />
       <Line type="monotone" dataKey="value" stroke="#8884d8" dot={false} />
     </LineChart>
   );
