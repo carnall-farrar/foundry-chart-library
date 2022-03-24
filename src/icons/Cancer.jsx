1fcbffa1cc6f285bce0d58f7588a8a82d5e1d161
zaharamiriam <miriam.zahara@carnallfarrar.com>
2022-03-24 17:00:04 +0000

    Add svg react components

---
+++ src/icons/Cancer.jsx
@@ -0,0 +1,18 @@
+const Cancer = () => {
+  return (
+    <svg
+      width={"24px"}
+      height={"24px"}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ paddingLeft: 5 }}
+    >
+      <path
+        d="M10,20H14V4H10V20M4,20H8V12H4V20M16,9V20H20V9H16Z"
+        fill={"#000"}
+      />
+    </svg>
+  );
+};
+
+export default Cancer;
