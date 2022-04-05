273cbd9562151307f2939fce475d16fa49676043
zaharamiriam <miriam.zahara@carnallfarrar.com>
2022-04-05 11:19:03 +0100

    Show hide tooltip when no data, remove hover state when no data.

--- src/components/TrendBarChart.jsx
+++ src/components/TrendBarChart.jsx
@@ -1,30 +1,45 @@
 const { BarChart, Tooltip, Bar, Cell } = Recharts;
 
-export const TrendBarChart = ({ data, width, height, cellData }) => {
+const TooltipContent = window.styled.div`
+background: white;
+padding: 0.5rem;
+border: 1px solid #555555;
+> p.label {
+  fontWeight: 700;
+  margin-bottom: 0.5rem;
+}
+`;
+
+export const TrendBarChart = ({ data, ambition, width, height, cellData }) => {
   const dateFormatter = (item) => dayjs(item).format("MMM D, YYYY");
   const filteredData = data.map((i) =>
     i.value === 0 || null ? { ...i, value: null } : i
   );
-  console.log(filteredData, "data");
 
   const CustomTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
+      const week = dayjs(payload[0].payload.date).format("MMM D, YYYY");
       return (
-        <div className="custom-tooltip">
-          <p className="label">{`${label} : ${payload[0].value}`}</p>
+        <TooltipContent>
+          <p className="label">{week}</p>
+          <p>{`${Math.round((payload[0].value * 100) / ambition) * 100}% `}</p>
           {/* <p className="intro">{getIntroOfPage(label)}</p> */}
-          <p className="desc">Anything you want can be displayed here.</p>
-        </div>
+        </TooltipContent>
       );
     }
 
     return null;
   };
 
+  const isNoData = filteredData.every((d) => d.value === null);
+  if (isNoData) {
+    return null;
+  }
+
   return (
     <BarChart width={width} height={height} data={filteredData}>
       <Tooltip
-        // content={<CustomTooltip />}
+        content={<CustomTooltip />}
         wrapperStyle={{ width: 100, height: 50 }}
         contentStyle={{ fontSize: 10 }}
         itemStyle={{ opacity: "0 !important" }}
@@ -32,7 +47,7 @@ export const TrendBarChart = ({ data, width, height, cellData }) => {
         position={{ x: -90 }}
         filterNull={true}
         active={false}
-        labelFormatter={(date) => dayjs(data[date].date).format("MMM D, YYYY")}
+        // labelFormatter={(date) => dayjs(data[date].date).format("MMM D, YYYY")}
       />
       <Bar dataKey="value">
         {data.map((entry, index) => (
