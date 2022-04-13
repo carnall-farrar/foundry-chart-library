af7d1247ac75b4a4b81511137308227090cc0a01
zaharamiriam <miriam.zahara@carnallfarrar.com>
2022-04-13 14:31:55 +0100

    Update colors of RAD rating to specification

--- src/colors.js
+++ src/colors.js
@@ -1,11 +1,11 @@
 export default {
-  red_light: "#FA999C",
-  red_dark: "#AC2F33",
-  amber_light: "#FFE79E",
-  amber_dark: "#FFBF00",
+  red_light: "#e87e76",
+  red_dark: "#DA291C",
+  amber_light: "#f4b966",
+  amber_dark: "#ED8B00",
   gray_dark: "#bdbdbd",
   gray_light: "#e7e7e7",
   white: "#FFFFFF",
-  green_light: "#a1e2c4",
-  green_dark: "#44a278",
+  green_light: "#66a390",
+  green_dark: "#006747",
 };
